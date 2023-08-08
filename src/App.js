7ba925b35f9b2487302672c677dd30079d82425c
SomethingSomethingOrOther <abdoulgdia@gmail.com>
2023-08-08 13:32:49 -0400

    committing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+
+const Star=()=>{
+    
+   const [stars,setStars]=useState([false,false,false,false,false])
+
+  const borderStar="https://www.svgrepo.com/show/532718/star-sharp.svg"
+  const filledStar="https://www.svgrepo.com/show/92788/favourites-filled-star-symbol.svg"
+
+  const handleHoverState=(index)=>{
+    const newStars=[...stars]
+    for (let i=0;i<=index;i++){
+      newStars[i]=true
+    }
+    setStars(newStars)
+  }
+  const handleMouseExit=()=>{
+      
+    setStars(stars.map(()=>false))
+  }
+
+  const handleClick=(index)=>{
+    const newStars=stars.map((_,i)=> i <= index )
+    setStars(newStars)
+  }
+
+
+  return (
+    <div>
+      {
+        stars.map((isFilled,index)=>(
+          <img  
+            key={index}
+            onMouseLeave={handleMouseExit}
+            onMouseEnter={()=>handleHoverState(index)}
+            onClick={()=>handleClick(index)}
+            className="borderStar"
+            style={{width:'2rem',heigh:'2rem'}}
+            src={isFilled ? filledStar:borderStar}
+            alt={isFilled ? "filledStar":"borderStar"}
+          />
+        ))
+      }
+      </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Star rating</h1>
       </header>
+      <div>
+        <Star />
+      </div>
     </div>
   );
 }
